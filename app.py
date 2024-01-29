93efa0ed486bc5f1908dd2e614c4b23efe732b73
UselessAmber <amberthequeer@gmail.com>
2024-01-29 16:07:17 +1100

    minor update with icons

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import tksheet as tks
 import tkinter as tk
 import library
 from library.database_interface import db_interface
 
 if __name__ == "__main__":
     master = tk.Tk()
-    master.title("Project Sekai Performance Tracker v1.1")
+    master.iconbitmap("app.ico")
+    master.title("Project Sekai Performance Tracker v1.12")
     app = db_interface(master)
     app.mainloop()
