91401fc942e05f9d9b5889809eea1d7b861b6ee1
UselessAmber <amberthequeer@gmail.com>
2024-01-29 16:24:49 +1100

    theme

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import tksheet as tks
 import tkinter as tk
+import tkinter.ttk as ttk
 import library
 from library.database_interface import db_interface
 
@@ -7,5 +8,9 @@ if __name__ == "__main__":
     master = tk.Tk()
     master.iconbitmap("app.ico")
     master.title("Project Sekai Performance Tracker v1.12")
+    style = ttk.Style(
+        master = master
+    )
+    style.theme_use("vista")
     app = db_interface(master)
     app.mainloop()
